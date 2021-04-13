9afd97519816e4881a6a0117bfde7c0c83747c1e
Samuel Anozie <samuelcanozie@gmail.com>
2021-04-13 20:01:10 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const http = require("http");
+const fs = require("fs");
+const url = "http://jsonplaceholder.typicode.com/posts";
+
+http.get(url, (res) => {
+  let content = "";
+  res.on("data", (data) => {
+    content += data;
+  });
+  res.on("end", () => {
+    fs.writeFile("result/post.json", content, (err) => {
+      if (err) throw err;
+      console.log("file successfully saved");
+    });
+  });
+});
