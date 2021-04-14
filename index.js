7bd260c6b3443846851c0313e9124cd2066851d2
Samuel Anozie <samuelcanozie@gmail.com>
2021-04-14 06:37:58 +0100

    update

--- index.js
+++ index.js
@@ -5,18 +5,22 @@ const fs = require("fs");
 const url = "http://jsonplaceholder.typicode.com/posts";
 
 //fetch data from api
-http.get(url, (res) => {
-  //initialize variable
-  let content = "";
-  res.on("data", (data) => {
-    //append incoming data to variable in streams
-    content += data;
-  });
-  res.on("end", () => {
-    //write data to file
-    fs.writeFile("result/posts.json", content, (err) => {
-      if (err) throw err;
-      console.log("file successfully saved");
+http
+  .get(url, (res) => {
+    //initialize variable
+    let content = "";
+    res.on("data", (data) => {
+      //append incoming data to variable in streams
+      content += data;
     });
+    res.on("end", () => {
+      //write data to file
+      fs.writeFile("result/posts.json", content, (err) => {
+        if (err) throw err;
+        console.log("file successfully saved");
+      });
+    });
+  })
+  .on("error", (err) => {
+    if (err) console.log("Error: ", err.message);
   });
-});
